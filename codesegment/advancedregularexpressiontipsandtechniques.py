04e601f62dc44a6a43202f4e4d1396d93bc2354e
demonlife <demonlife2013@gmail.com>
2014-04-28 11:08:54 +0800

    modify python.org

--- codesegment/advancedregularexpressiontipsandtechniques.py
+++ codesegment/advancedregularexpressiontipsandtechniques.py
@@ -86,8 +86,38 @@ print "f* => {0}".format(pattern.group(1))
 print "b* => {0}".format(pattern.group(2))
 '''
 
+'''
 # named subpatterns
 string = "hello foobar"
 pattern = re.search(r'(?P<fstart>f.*)(?P<bstart>b.*)', string)
 print "f* => {0}".format(pattern.group('fstart'))
 print "b* => {0}".format(pattern.group('bstart'))
+'''
+
+#callback
+
+template = "Hello [first_name] [last_name],\
+Thank you for purchasing [product_name] from [store_name].\
+The total cost of your purchase was [product_price] plus [ship_price] for shipping.\
+You can expect your product to arrive in [ship_days_min] to [ship_days_max] business days.\
+Sincerely, [store_manager_name]"
+
+dic = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "product_name": "iphone",
+    "store_name" : "Walkers",          
+    "product_price": "$500",          
+    "ship_price": "$10",          
+    "ship_days_min": "1",          
+    "ship_days_max": "5",          
+    "store_manager_name": "DoeJohn"          
+}          
+
+result = re.compile(r'\[(.*)\]')
+#print result.sub("John", template, count=1)
+
+def multiple_replace(dic, text):
+    pattern = "|".join(map(lambda key: re.escape("[" + key + "]"), dic.keys()))
+    return re.sub(pattern, lambda m: dic[m.group()[1:-1]], text)
+print multiple_replace(dic, template)
