77c13cd1f04808a44992ad7a167fd78f7a7ae9cf
demonlife <demonlife2013@gmail.com>
2014-05-23 18:40:10 +0800

    modify files

---
+++ codesegment/printjson.py
@@ -0,0 +1,15 @@
+#encoding: utf8
+
+import json
+
+class MyJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
+        elif isinstance(obj, date):
+            return obj.strftime('%Y-%m-%d')
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+# 使用
+# json.dumps(datalist, cls=MyJsonEncoder)
