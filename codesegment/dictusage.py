ad0385c53467ebef0347663871a5d1b078aeaca4
demonlife2013@gmail.com <demonlife2013@gmail.com>
2014-07-02 17:23:52 +0800

    add dict usage

---
+++ codesegment/dictusage.py
@@ -0,0 +1,12 @@
+#encoding:utf8
+
+#字典排序
+person = [{'name':'aab', 'age':10, 'cnt':100},
+          {'name':'aab', 'age':22, 'cnt':89},
+          {'name':'aab', 'age':22, 'cnt':98},
+          {'name':'abc', 'age':11, 'cnt':68},
+          {'name':'aab', 'age':25, 'cnt':20}]
+# 按姓名的升序排序，如果姓名相同则按年龄的降序排列， 然后按cnt升序排列
+d = sorted(person, key=lambda x:(x['name'], -x['age'], x['cnt']))
+print d
+
