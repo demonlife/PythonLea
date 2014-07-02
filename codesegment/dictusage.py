3252bfb2997a339afa28c1683bad46d63acf21a2
demonlife2013@gmail.com <demonlife2013@gmail.com>
2014-07-02 17:39:00 +0800

    modify dictusage.py

--- codesegment/dictusage.py
+++ codesegment/dictusage.py
@@ -1,6 +1,6 @@
 #encoding:utf8
 
-#字典排序, 使用多个字段排序
+#list中的字典排序, 使用多个字段排序
 person = [{'name':'aab', 'age':10, 'cnt':100},
           {'name':'aab', 'age':22, 'cnt':89},
           {'name':'aab', 'age':22, 'cnt':98},
@@ -9,4 +9,22 @@ person = [{'name':'aab', 'age':10, 'cnt':100},
 # 按姓名的升序排序，如果姓名相同则按年龄的降序排列， 然后按cnt升序排列
 d = sorted(person, key=lambda x:(x['name'], -x['age'], x['cnt']))
 print d
+
+# 单独对字典排序
+from operator import itemgetter
+phonebook = {'Linda':'7750', 'Bob': '9345', 'Carol':'5834'}
+
+sorted_pb = sorted(phonebook.iteritems(), key=itemgetter(1))
+
+# 多维list排序
+gameresult = [['Bob', 95, 'A'],['Alan', 86, 'C'], ['Mandy', 82.5, 'A']]
+sorted(gameresult, key=itemgetter(2, 1))
+
+# 字典中混合list排序
+
+mydict = {'Li':['M', 7],
+          'Zhang': ['E', 2],
+          'Wang': ['P', 3]
+      }
+sorted(mydict.iteritems(), key=lambda (k,v): itemgetter(1)(v))
 
